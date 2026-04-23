1b565eaff2c5c63b2a10267a8f9368cfd3676380
John Kibirige <john@example.com>
2026-04-23 09:01:38 +0800

    implement logic or the card grid

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -11,6 +11,7 @@ import RandomSVG from "../svgs/RandomSVG";
 import Humberger from "../svgs/Humberger";
 import Close from "../svgs/Close";
 import { characterDatabase } from "../data/database";
+import GridIcon from "../svgs/GridIcon";
 
 const Dashboard = ({ selectedLevel, setBackButtonClicked }) => {
   const [cardPosition, setCardPosition] = useState(0);
@@ -19,6 +20,8 @@ const Dashboard = ({ selectedLevel, setBackButtonClicked }) => {
   // state for displaying the navigation popup
   const [showMenu, setShowMenu] = useState(false);
   const [activeCategory, setActiveCategory] = useState("main");
+  // grid logic
+  const [showCardGrid, setShowCardGrid] = useState(false);
 
   const resetCardState = () => {
     setShowPinyin(false);
@@ -26,6 +29,7 @@ const Dashboard = ({ selectedLevel, setBackButtonClicked }) => {
   };
 
   const dataBasedOnLevel = characterDatabase[selectedLevel] || {};
+
   const data = dataBasedOnLevel[activeCategory] || [];
 
   const numberOfCards = data.length;
@@ -141,6 +145,14 @@ const Dashboard = ({ selectedLevel, setBackButtonClicked }) => {
             </button>
           ))}
         </nav>
+
+        {/* grid buttons */}
+        <button
+          className="cursor-pointer active:scale-95 ml-5"
+          onClick={() => setShowCardGrid((prevState) => !prevState)}
+        >
+          <GridIcon />
+        </button>
       </header>
       {/* overlay for mobile */}
       {showMenu && (
@@ -149,54 +161,86 @@ const Dashboard = ({ selectedLevel, setBackButtonClicked }) => {
           className="fixed lg:hidden inset-0 bg-black/50 backdrop-blur-sm z-40"
         />
       )}
-      <div className="   PLEASE REMOVE ME REMOVE ME REMOVE ME..........................................">
-        <div className="select-text">
-          <Card
-            key={cardPosition}
-            cardPosition={cardPosition}
-            numberOfCards={numberOfCards}
-            name={name}
-            pinyin={pinyin}
-            partOfSpeech={partOfSpeech}
-            meaning={meaning}
-            example={example}
-            examplePinyin={examplePinyin}
-            translation={translation}
-            showPinyin={showPinyin}
-            showMeaning={showMeaning}
-            setShowPinyin={setShowPinyin}
-            setShowMeaning={setShowMeaning}
-          />
+      {/* grid content */}
+      {showCardGrid ? (
+        <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 gap-4 md:gap-5 lg:gap-6 xl:px-5">
+          {data.map((obj, index) => (
+            <button
+              key={index}
+              className={`aspect-square rounded-lg shadow-sm hover:shadow-md hover:scale-105 active:scale-95 transition-all duration-200 flex flex-col items-center justify-center relative ${
+                index === cardPosition
+                  ? "bg-orange-600 text-white"
+                  : "bg-white text-zeidark hover:bg-cyan-100"
+              }`}
+              onClick={() => {
+                setCardPosition(index);
+                setShowCardGrid(false);
+                resetCardState();
+              }}
+            >
+              <span
+                className={`block text-sm sm:text-base xl:text-lg px-2 py-1 absolute left-0 top-0 ${
+                  index === cardPosition ? "text-white" : "text-orange-500"
+                }`}
+              >
+                {index + 1}
+              </span>
+              <span className="text-xl lg:text-3xl xl:5xl tracking-wide font-medium font-kaiti">
+                {obj.name}
+              </span>
+            </button>
+          ))}
         </div>
-        {/* create the next and previous button */}
-        <div className=" flex justify-between sm:justify-around lg:justify-between w-full lg:w-2/3 xl:w-1/2 lg:mx-auto mt-6 lg:mt-7 xl:mt-8 ">
-          <button
-            className=" bg-orange-600 px-4 py-2 rounded-md flex gap-2 items-center active:scale-95 lg:text-lg shadow-md cursor-pointer"
-            onClick={handlePrevious}
-          >
-            <ChevronBack />
-            <span className="hidden sm:block text-white">Previous</span>
-          </button>
+      ) : (
+        <div className="   PLEASE REMOVE ME REMOVE ME REMOVE ME..........................................">
+          <div className="select-text">
+            <Card
+              key={cardPosition}
+              cardPosition={cardPosition}
+              numberOfCards={numberOfCards}
+              name={name}
+              pinyin={pinyin}
+              partOfSpeech={partOfSpeech}
+              meaning={meaning}
+              example={example}
+              examplePinyin={examplePinyin}
+              translation={translation}
+              showPinyin={showPinyin}
+              showMeaning={showMeaning}
+              setShowPinyin={setShowPinyin}
+              setShowMeaning={setShowMeaning}
+            />
+          </div>
+          {/* create the next and previous button */}
+          <div className=" flex justify-between sm:justify-around lg:justify-between w-full lg:w-2/3 xl:w-1/2 lg:mx-auto mt-6 lg:mt-7 xl:mt-8 ">
+            <button
+              className=" bg-orange-600 px-4 py-2 rounded-md flex gap-2 items-center active:scale-95 lg:text-lg shadow-md cursor-pointer"
+              onClick={handlePrevious}
+            >
+              <ChevronBack />
+              <span className="hidden sm:block text-white">Previous</span>
+            </button>
 
-          <button
-            className=" bg-zeidark px-4 py-2 rounded-md flex gap-2 items-center active:scale-95 lg:text-lg shadow-md cursor-pointer"
-            onClick={handleRandom}
-          >
-            <RandomSVG />
-            <span className="hidden sm:block text-white">Shuffle</span>
-          </button>
+            <button
+              className=" bg-zeidark px-4 py-2 rounded-md flex gap-2 items-center active:scale-95 lg:text-lg shadow-md cursor-pointer"
+              onClick={handleRandom}
+            >
+              <RandomSVG />
+              <span className="hidden sm:block text-white">Shuffle</span>
+            </button>
 
-          <button
-            className=" border-2 border-orange-600  px-4 py-2 rounded-md flex gap-4 items-center active:scale-95 shadow-md cursor-pointer"
-            onClick={handleNext}
-          >
-            <span className=" hidden sm:block text-orange-600 lg:text-lg">
-              Next
-            </span>{" "}
-            <ChevronFront />
-          </button>
+            <button
+              className=" border-2 border-orange-600  px-4 py-2 rounded-md flex gap-4 items-center active:scale-95 shadow-md cursor-pointer"
+              onClick={handleNext}
+            >
+              <span className=" hidden sm:block text-orange-600 lg:text-lg">
+                Next
+              </span>{" "}
+              <ChevronFront />
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </section>
   );
 };
